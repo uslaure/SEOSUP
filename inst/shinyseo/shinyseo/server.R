773914e4fe6a1becc46fcca906ea3b48a7a97681
uslaure <laure-ashley.graimont@supinternet.fr>
2016-12-21 15:51:47 +0100

    Add Config interface

--- inst/shinyseo/shinyseo/server.R
+++ inst/shinyseo/shinyseo/server.R
@@ -3,11 +3,30 @@ library(SEO)
 library(shiny)
 library(DT)
 library(dygraphs)
+library(ggplot2)
 
 
 shinyServer(function(input, output, session) {
-
-
+  randomVals <- eventReactive(input$go, {
+    runif(input$n)
+  })
+  output$plot <- renderPlot({
+    hist(randomVals())
+  })
+  output$value <- renderText({ input$texte})
+  output$table <- DT::renderDataTable(DT::datatable({
+    data <- mpg
+    if (input$man != "All") {
+      data <- data[data$manufacturer == input$man,]
+    }
+    if (input$cyl != "All") {
+      data <- data[data$cyl == input$cyl,]
+    }
+    if (input$trans != "All") {
+      data <- data[data$trans == input$trans,]
+    }
+    data
+  }))
 
 })
 
--- inst/shinyseo/shinyseo/ui.R
+++ inst/shinyseo/shinyseo/ui.R
@@ -5,13 +5,41 @@ library(DT)
 library(dygraphs)
 library(shinyjs)
 library(rhandsontable)
+library(ggplot2)
 
 fluidPage(
   useShinyjs(), # on active shinyjs
   titlePanel("Shiny SEO"),
   tabsetPanel(
-    tabPanel("research",
-             titlePanel("Recherches")
+    tabPanel("Configuration",
+             textInput("texte", "Votre recherche", "Data Summary"),
+             verbatimTextOutput("value"),
+             dateRangeInput(inputId = "plagedate", label = " Votre recherche sur la durée: ", language = "fr"),
+             actionButton("go", "Go"),
+             numericInput("n", "Plot de votre recherche web", 10),
+             plotOutput("plot"),
+             tableOutput(outputId = "table1"),
+             column(4,
+                    selectInput("man",
+                                "Manufacturer:",
+                                c("All",
+                                  unique(as.character(mpg$manufacturer))))
+             ),
+             column(4,
+                    selectInput("trans",
+                                "Transmission:",
+                                c("All",
+                                  unique(as.character(mpg$trans))))
+             ),
+             column(4,
+                    selectInput("cyl",
+                                "Cylinders:",
+                                c("All",
+                                  unique(as.character(mpg$cyl))))
+             )
+    ),
+    fluidRow(
+      DT::dataTableOutput("table")
     ),
     tabPanel("site",
              titlePanel("Sites")
